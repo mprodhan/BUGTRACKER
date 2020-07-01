c1bfc99f0b18638a3d579bd8f8a739ca7dff8bd2
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-07-01 11:41:29 -0400

    added a minimized version of homepage and tested the authentication views to access homepage. Test passed. Moving to create ticket submission views on a seperate branch submit.

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from ticket import views
 
-urlpatterns = []
+urlpatterns = [
+    path('home', views.index, name='homepage')
+]
 
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from ticket.models import Ticket
 
-# Create your views here.
+def index(request):
+    # data = Ticket.objects.all()
+    return render(request, "index.html", {})
