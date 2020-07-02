8ecdcb7f92808aa3efba6a237dc92c4177f3d4d9
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-07-02 11:29:57 -0400

    created the edit function and tested the fucntionality. Test passed and the edit button renders the edit section for each individual ticket.

--- ticket/views.py
+++ ticket/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from ticket.models import Ticket
 
 def index(request):
-    # data = Ticket.objects.all()
-    return render(request, "index.html", {})
+    data = Ticket.objects.all()
+    return render(request, "index.html", {"data": data})
